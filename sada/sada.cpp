8acd8e6bb40027274b1ebeb3c26437fdb40df14c
Kurt <iskander.taniev3@gmail.com>
2025-01-29 17:53:51 -0500

    naming changes.

--- sada/sada.cpp
+++ sada/sada.cpp
@@ -2,13 +2,11 @@
 using namespace std;
 
 int main() {
-    int variable = 42;
-    int* ptr = &variable; // Pointer stores the address of variable
-
-    cout << "Address of variable: " << ptr << endl;  // Address stored in ptr
-    cout << "Value of variable: " << *ptr << endl;   // Dereference to access value
-
-    // Dynamic memory allocation
-    ptr = new int(100); // Allocate memory
-    cout << "Dynamically allocated value: " << *ptr << endl;
+    int* p;
+    if (true)
+    {
+        int a = 5;
+        p = &a;
+    }
+    std::cout <<"it really compiled this:"<< * p;
 }
